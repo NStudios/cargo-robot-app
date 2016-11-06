2f8c2549ca3951226eeeb7ccc07006968e3fb317
marulka <nikola.georgiev@mail.bg>
2016-11-07 00:14:42 +0100

    Many things are done, few left to complite.
    Signed-off-by: marulka <nikola.georgiev@mail.bg>

--- cargo-robot-app/src/com/abc/cra/Map.java
+++ cargo-robot-app/src/com/abc/cra/Map.java
@@ -13,6 +13,11 @@ public class Map {
 	
 	public static final char WALL = '#';
 	public static final char EMPTY_SPACE = '.';
+	public static final int GO_LEFT = -1;
+	public static final int GO_DOWN = 0;
+	public static final int GO_RIGHT = 1;
+	
+	char[][] storageMap;
 
 	public static EMapElement checkElement(char element) {
 		switch (element) {
@@ -27,5 +32,19 @@ public class Map {
 		}
 		
 		return null;
+	}
+
+	/**
+	 * @return the storageMap
+	 */
+	public char[][] getStorageMap() {
+		return this.storageMap;
+	}
+
+	/**
+	 * @param storageMap the storageMap to set
+	 */
+	public void setStorageMap(char[][] storageMap) {
+		this.storageMap = storageMap;
 	}
 }
--- cargo-robot-app/src/com/abc/cra/PathFinder.java
+++ cargo-robot-app/src/com/abc/cra/PathFinder.java
@@ -4,6 +4,7 @@
 package com.abc.cra;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,16 +18,137 @@ import java.util.List;
 public class PathFinder {
 
 	private char[] rawMap;
+	private Map storage;
 	private List<Integer> xDimList;
+	private List<Point[]> allPosiblePathslist;
 
 	public PathFinder() {
+		this.storage = new Map();
 		this.xDimList = new LinkedList<Integer>();
+		this.allPosiblePathslist = new LinkedList<Point[]>();
 	}
 
 	public char[] findOptimalPath(char[] map) {
 		char[][] mapArray = convertStringToArray(map);
-		
+
+		if (this.storage != null) {
+			this.storage.setStorageMap(mapArray);
+		}
+
+		Integer entrance = null;
+		if (mapArray != null) {
+			entrance = findEntrance(mapArray);
+		} else {
+			return null;
+		}
+
+		if (entrance != null) {
+			return findShortestPath(entrance);
+		}
+
 		return null;
+	}
+
+	private Integer findEntrance(char[][] mapArray) {
+		for (int i = 0; i < mapArray[0].length; i++) {
+			if (mapArray[i][0] == Map.EMPTY_SPACE) {
+				Integer entranceElement = new Integer(i + 1);
+
+				System.out.println("Entrance is element: " + entranceElement);
+
+				return entranceElement;
+			}
+		}
+
+		// In case of no entrance, return null
+		return null;
+	}
+
+	private char[] findShortestPath(Integer entrance) {
+		Point entrancePoint = new Point(entrance.intValue(), 0);
+
+		travelThroughMap(entrancePoint, new LinkedList<Point>(), Map.GO_DOWN);
+
+		return null;
+	}
+
+	private void travelThroughMap(Point startPoint, List<Point> path, int lastDirection) {
+		Runnable runn = new Runnable() {
+			@Override
+			public void run() {
+				moveOnMap(startPoint, path, lastDirection);
+			}
+		};
+		Thread t = new Thread(runn);
+		t.start();
+	}
+
+	protected void moveOnMap(Point point, List<Point> path, int lastDirection) {
+		int exitRow = this.storage.getStorageMap().length;
+		boolean hasOptions = true;
+		boolean isGone = false;
+		List<Point> alreadyDrovePath = path;
+		Point startPoint = point;
+
+		while (hasOptions || !isGone) {
+			Point p = makeStep(startPoint, alreadyDrovePath, lastDirection);
+			if (p.equals(startPoint)) {
+				hasOptions = false;
+			}
+			if (p.y == exitRow) {
+				isGone = true;
+			}
+			startPoint = p;
+		}
+	}
+
+	private Point makeStep(Point element, List<Point> adp, int lastDirection) {
+		boolean hasOption = false;
+		char[][] map = this.storage.getStorageMap();
+		Point newPoint = new Point(element.x, element.y);
+
+		if (lastDirection != Map.GO_LEFT
+				&& map[newPoint.x - 1][element.y] == Map.EMPTY_SPACE) {
+			hasOption = true;
+			Point p = new Point((newPoint.x - 1), element.y);
+			newPoint = p;
+		}
+		if (map[newPoint.x][element.y + 1] == Map.EMPTY_SPACE) {
+			Point p = new Point(newPoint.x, (element.y + 1));
+			
+			if (hasOption) {
+				List<Point> l = new LinkedList<Point>();
+				for (Iterator<Point> it = adp.iterator(); it.hasNext();) {
+					Point point = (Point) it.next();
+					l.add(point);
+				}
+				
+				travelThroughMap(p, l, Map.GO_DOWN);
+			} else {
+				hasOption = true;
+				newPoint = p;
+			}
+		}
+		if (lastDirection != Map.GO_RIGHT
+				&& map[newPoint.x + 1][element.y] == Map.EMPTY_SPACE) {
+			Point p = new Point((newPoint.x + 1), (element.y));
+			
+			if (hasOption) {
+				List<Point> l = new LinkedList<Point>();
+				for (Iterator<Point> it = adp.iterator(); it.hasNext();) {
+					Point point = (Point) it.next();
+					l.add(point);
+				}
+				
+				travelThroughMap(p, l, Map.GO_RIGHT);
+			} else {
+				hasOption = true;
+				newPoint = p;
+			}
+
+		}
+
+		return newPoint;
 	}
 
 	private char[][] convertStringToArray(char[] mapArray) {
@@ -75,11 +197,11 @@ public class PathFinder {
 				newMapArray[tmpI] = mapArray[i];
 				tmpI++;
 			}
-			
+
 			xDimTmp++;
 		}
 		result = xDimTmp;
-		
+
 		for (Iterator<Integer> it = this.xDimList.iterator(); it.hasNext();) {
 			Integer intgr = (Integer) it.next();
 			int tmpVar = intgr.intValue();
@@ -90,7 +212,7 @@ public class PathFinder {
 		for (int i = 0; i < this.rawMap.length; i++) {
 			this.rawMap[i] = newMapArray[i];
 		}
-		
+
 		return result;
 	}
 
